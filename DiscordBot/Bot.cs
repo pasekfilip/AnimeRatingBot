892463155f55a5ed5254800d4b292ee55fb12395
Filip Pašek <filip.pasek@uniprog.cz>
2022-07-01 17:53:22 +0200

    Add project files.

---
+++ DiscordBot/Bot.cs
@@ -0,0 +1,107 @@
+﻿using DiscordBot.Commands;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+        public InteractivityExtension Interactivity { get; private set; }
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+            {
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+            }
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug
+            };
+            Client = new DiscordClient(config);
+
+
+            Client.Ready += OnClientReady;
+            Client.MessageCreated += OnMessageCreated;
+            Client.MessageUpdated += OnMessageUpdate;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableMentionPrefix = true,
+                EnableDms = false,
+                DmHelp = true
+            };
+
+            Client.UseInteractivity(new InteractivityConfiguration()
+            {
+                Timeout = TimeSpan.FromMinutes(3)
+            });
+
+
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+            Commands.RegisterCommands<MainCommands>();
+            await Client.ConnectAsync();
+
+
+
+            await Task.Delay(-1);
+        }
+
+
+        private async Task OnMessageCreated(DiscordClient client, MessageCreateEventArgs e)
+        {
+            var animeToRate = await client.GetChannelAsync(849971302797279265);
+            var mee6 = await client.GetUserAsync(159985870458322944);
+
+            if (e.Channel == animeToRate && e.Author == mee6)
+            {
+                var namesOfEmojies = new string[] { ":Rate5outof5:", ":Rate4outof5:", ":Rate3outof5:", ":Rate2outof5", ":Rate1outof5:" };
+                var listOfRatings = new List<DiscordEmoji>();
+                var message = e.Message;
+                foreach (var nameOfEmoji in namesOfEmojies)
+                {
+                    listOfRatings.Add(DiscordEmoji.FromName(client, nameOfEmoji));
+                }
+                
+                await message.CreateReactionAsync(listOfRatings[0]);
+                await message.CreateReactionAsync(listOfRatings[1]);
+                await message.CreateReactionAsync(listOfRatings[2]);
+                await message.CreateReactionAsync(listOfRatings[3]);
+                await message.CreateReactionAsync(listOfRatings[4]);
+
+            }
+        }
+        public async Task OnMessageUpdate(DiscordClient client, MessageUpdateEventArgs e)
+        {
+            var addedAnime = await client.GetChannelAsync(850711592957509633);
+            await addedAnime.SendMessageAsync(e.Message.Embeds[0].Title + ": " + e.Message.JumpLink).ConfigureAwait(false);
+        }
+
+        private Task OnClientReady(DiscordClient client, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DiscordBot/Commands/MainCommands.cs
@@ -0,0 +1,64 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands
+{
+    public class MainCommands : BaseCommandModule
+    {
+        [Command("rate")]
+        public async Task Rate(CommandContext ctx)
+
+        {
+            var dd = DiscordEmoji.FromGuildEmote(ctx.Client, 850708320830226503);
+            var interactivity = ctx.Client.GetInteractivity();
+            var anime = await ctx.Client.GetChannelAsync(849971302797279265);
+            var addedAnime = await ctx.Client.GetChannelAsync(850711592957509633);
+            var namesOfEmojis = new string[] { ":Rate5outof5:", ":Rate4outof5:", ":Rate3outof5:", ":Rate2outof5:", ":Rate1outof5:" };
+            var listOfRatings = new List<DiscordEmoji>();
+            
+            var message = await interactivity.WaitForMessageAsync(x => x.Channel.Id == anime.Id).ConfigureAwait(false);
+
+
+            foreach (var item in namesOfEmojis)
+            {
+                
+                listOfRatings.Add(DiscordEmoji.FromName(ctx.Client, item));
+            }
+
+            await message.Result.CreateReactionAsync(listOfRatings[0]).ConfigureAwait(false);
+            await message.Result.CreateReactionAsync(listOfRatings[1]).ConfigureAwait(false);
+            await message.Result.CreateReactionAsync(listOfRatings[2]).ConfigureAwait(false);
+            await message.Result.CreateReactionAsync(listOfRatings[3]).ConfigureAwait(false);
+            await message.Result.CreateReactionAsync(listOfRatings[4]).ConfigureAwait(false);
+
+            await addedAnime.SendMessageAsync(message.Result.Embeds[0].Title + ": " + message.Result.JumpLink).ConfigureAwait(false);
+
+
+            await ctx.Channel.DeleteMessageAsync(ctx.Message);
+        }
+
+        [Command("clear")]
+        public async Task Clear(CommandContext ctx, int howManyMessages)
+        {
+            await DeleteCommand(ctx);
+
+            var messages = await ctx.Channel.GetMessagesAsync(howManyMessages).ConfigureAwait(false);
+            await ctx.Channel.DeleteMessagesAsync(messages);
+        }
+
+
+        private async Task DeleteCommand(CommandContext ctx)
+        {
+            var command = ctx.Message;
+            await ctx.Channel.DeleteMessageAsync(command);
+        }
+    }
+}
